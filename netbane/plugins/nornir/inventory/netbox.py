5ec526959adc58be54741041f7f86c72fa11799e
Austin de Coup-Crank <Austin.deCoupCrank@moodys.com>
2022-02-17 14:45:00 -0600

    transform tags to flat list

--- netbane/plugins/nornir/inventory/netbox.py
+++ netbane/plugins/nornir/inventory/netbox.py
@@ -1,8 +1,13 @@
 from nornir.core.inventory import Host
-
+import copy
 
 def netbox_transform(host: Host) -> None:
     # store raw netbox data in separate key in case we want it later
-    host.data["netbox"] = host.data
+    host.data["netbox"] = copy.deepcopy(host.data)
     netbox = host.data["netbox"]
     host.data["site"] = netbox["site"]["slug"].lower()
+
+    # make tags a flat list of slugs
+    host.data['tags'] = []
+    for tag in netbox['tags']:
+        host.data['tags'].append(tag['slug'])
